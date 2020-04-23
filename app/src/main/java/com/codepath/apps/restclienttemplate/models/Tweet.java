3753a27f404be6e5ca313cb16cc49a5709a163f2
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2020-04-22 23:02:04 -0400

    Set up room

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -1,5 +1,10 @@
 package com.codepath.apps.restclienttemplate.models;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -7,11 +12,23 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
 public class Tweet {
 
-    public String body;
-    public String createdAt;
+    @ColumnInfo
+    @PrimaryKey
     public long id;
+
+    @ColumnInfo
+    public String body;
+
+    @ColumnInfo
+    public String createdAt;
+
+    @ColumnInfo
+    public long userId;
+
+    @Ignore
     public User user;
 
     public static Tweet fromJson(JSONObject jsonObject) throws JSONException {
@@ -19,7 +36,9 @@ public class Tweet {
          tweet.body = jsonObject.getString("text");
          tweet.createdAt = jsonObject.getString("created_at");
          tweet.id = jsonObject.getLong("id");
-         tweet.user = User.fromJson(jsonObject.getJSONObject("user"));
+         User user = User.fromJson(jsonObject.getJSONObject("user"));
+         tweet.user = user;
+         tweet.userId = user.id;
          return tweet;
     }
 
--- app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
@@ -1,20 +1,31 @@
 package com.codepath.apps.restclienttemplate.models;
 
-import org.json.JSONArray;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
-import java.util.List;
-
+@Entity
 public class User {
 
+    @ColumnInfo
+    @PrimaryKey
+    public long id;
+
+    @ColumnInfo
     public String name;
+
+    @ColumnInfo
     public String screenName;
+
+    @ColumnInfo
     public String profileImageUrl;
 
     public static User fromJson(JSONObject jsonObject) throws JSONException {
      User user = new User();
+     user.id = jsonObject.getLong("id");
      user.name = jsonObject.getString("name");
      user.screenName = jsonObject.getString("screen_name");
      user.profileImageUrl = jsonObject.getString("profile_image_url_https");
